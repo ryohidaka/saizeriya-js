01a4feb0c5e5be7293807ea15bbea396af47e130
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-08-14 23:30:59 +0900

    test: getCategories() のテストコードを追加

---
+++ src/lib/category.test.ts
@@ -0,0 +1,50 @@
+import { describe, it, expect } from "vitest";
+
+import { Menu } from "@/types";
+import { getCategories } from "./category";
+
+const mockMenus: Menu[] = [
+  {
+    calorie: 192,
+    category: "グランド",
+    categoryEn: "Grand Menu",
+    categoryZh: "主",
+    genre: "サラダ",
+    id: 1202,
+    name: "小エビのサラダ",
+    nameEn: "Shrimp Salad",
+    nameZh: "小甜虾沙拉",
+    price: 319,
+    priceWithTax: 350,
+    salt: 1.5,
+  },
+  {
+    calorie: undefined,
+    category: "キッズ",
+    categoryEn: "Kids",
+    categoryZh: "孩子们",
+    genre: "ドリンクバー",
+    id: 5102,
+    name: "キッズドリンクバー",
+    nameEn: "Drink Bar",
+    nameZh: "畅饮吧",
+    price: 91,
+    priceWithTax: 100,
+    salt: undefined,
+  },
+];
+
+describe("getCategories", () => {
+  it("カテゴリを正しく取得できるか", () => {
+    const categories = getCategories(mockMenus);
+    expect(categories).toEqual([
+      { category: "グランド", categoryEn: "Grand Menu", categoryZh: "主" },
+      { category: "キッズ", categoryEn: "Kids", categoryZh: "孩子们" },
+    ]);
+  });
+
+  it("空の配列を渡した場合、空の配列が返るか", () => {
+    const categories = getCategories([]);
+    expect(categories).toEqual([]);
+  });
+});
