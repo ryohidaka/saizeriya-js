82436c2d0e9424907983a865c2ba2987067f80ff
ryohidaka <39184410+ryohidaka@users.noreply.github.com>
2024-08-15 23:41:53 +0900

    test: Saizeriya クラスのテストコードを追加

---
+++ tests/Saizeriya.test.ts
@@ -0,0 +1,51 @@
+import { describe, it, expect, beforeEach } from "vitest";
+
+import { Saizeriya } from "../src";
+
+describe("Saizeriya", () => {
+  let saizeriya: Saizeriya;
+
+  beforeEach(() => {
+    saizeriya = new Saizeriya();
+  });
+
+  it("初期化時にメニューをロードすること", async () => {
+    await saizeriya.all();
+  });
+
+  it("フィルタリングされた全てのメニューを返すこと", async () => {
+    const params = { genres: ["サラダ"] };
+    await saizeriya.all(params);
+  });
+
+  it("全てのカテゴリを返すこと", async () => {
+    await saizeriya.categories();
+  });
+
+  it("全てのジャンルを返すこと", async () => {
+    await saizeriya.genres();
+  });
+
+  it("IDに対応するメニューを返すこと", async () => {
+    const menu = await saizeriya.getById(1202);
+    expect(menu).toEqual({
+      calorie: 192,
+      category: "グランド",
+      categoryEn: "Grand Menu",
+      categoryZh: "主",
+      genre: "サラダ",
+      id: 1202,
+      name: "小エビのサラダ",
+      nameEn: "Shrimp Salad",
+      nameZh: "小甜虾沙拉",
+      price: 319,
+      priceWithTax: 350,
+      salt: 1.5,
+    });
+  });
+
+  it("存在しないIDに対してundefinedを返すこと", async () => {
+    const menu = await saizeriya.getById(9999);
+    expect(menu).toBeUndefined();
+  });
+});
